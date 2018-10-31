69db7450ff9a3d26ea38dceb80050f18422664fc
slugbucket <julianrawcliffe@gmail.com>
2018-10-31 21:29:01 +0000

    A fw more tests and a list of pips for Jenkins build purposes

--- crossword-hints-tests.py
+++ crossword-hints-tests.py
@@ -83,6 +83,8 @@ class XwordhintsTestCase(unittest.TestCase):
     """        S E T T E R   T Y P E S         """
     new_setter_type = {name: "Convoluted",
                        description: "Seriously devious and mindbending clues"}
+    upd_setter_type = {name: "Fiendish",
+                       description: "Seriously devious and mindbending clues"}
 
     def test_000_count_setter_types(self):
         print("DEBUG: Test full and empty setter types")
@@ -101,7 +103,19 @@ class XwordhintsTestCase(unittest.TestCase):
         print("DEBUG: Submit a new setter type.")
         rv = self.app.post('/setter-types/new', new_setter_type)
         assert b'Saved new setter type, ' in rv.data'
+        nr = crossword_hints.setter_types.select().count()
+        print("DEBUG: Found %s rows in the table." % nr)
+        assert nr == 6
 
+    def test_003_edit_setter_type(self):
+        print("DEBUG: Update a setter type.")
+        rv = self.app.post('/setter-types/new', upd_setter_type)
+        assert b'Updated setter type, Fiendish' in rv.data'
+
+    def test_004_delete_setter_type(self):
+        print("DEBUG: Update a setter type.")
+        rv = self.app.post('/setter-types/new', upd_setter_type)
+        assert b'Updated setter type, Fiendish' in rv.data'
 
     def test_099_clear_data(self):
         self.clearSampleData()
