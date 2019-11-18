6480e9882ac1e59a149bff4580c11e1dc5a4f2ba
slugbucket <julianrawcliffe@gmail.com>
2019-11-18 19:12:12 +0000

    feat0025 blocked on making updates because of an unfathomable missing _meta in the users class. Cannot see any way to continue beyond this and none of the unit tests can currently be run

---
+++ crossword_hints/tests/users.sql
@@ -0,0 +1,4 @@
+REPLACE INTO users(username, created_at, updated_at) VALUES
+('unittest', datetime('now'), datetime('now')),
+('integrationtest', datetime('now'), datetime('now')),
+('smoketest', datetime('now'), datetime('now'));
