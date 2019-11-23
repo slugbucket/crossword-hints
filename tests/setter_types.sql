b78a6509a23a5a2353b1667c7da04cdf6e288b56
slugbucket <julianrawcliffe@gmail.com>
2019-11-23 15:16:33 +0000

    feat0025 removed the top-level templates and tests directorieies that have been copied into the module

--- tests/setter_types.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO setter_types(name, description, created_at, updated_at)
-VALUES
-('Normal', 'Good mix of anagrams', datetime('now'), datetime('now')),
-('Partial', 'Partial anagrams requiring subclues', datetime('now'), datetime('now')),
-('Sly', 'No anagrams, lots of mis-direction', datetime('now'), datetime('now')),
-('Hard', 'No obvious match between clues and solutions', datetime('now'), datetime('now')),
-("Don't bother", 'No need to waste time trying to decipher', datetime('now'), datetime('now'));
--- tests/users.sql
+++
@@ -1,4 +0,0 @@
-REPLACE INTO users(username, created_at, updated_at) VALUES
-('unittest', datetime('now'), datetime('now')),
-('integrationtest', datetime('now'), datetime('now')),
-('smoketest', datetime('now'), datetime('now'));
